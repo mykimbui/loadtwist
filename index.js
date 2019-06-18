7de9dd3aaf202e1e9e195e6ec88d493d4004ec1c
My Kim Bui <mykimbuibui@gmail.com>
2019-06-18 10:21:07 +0900

    add delay

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ sectionTag.appendChild(renderer.domElement)
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000)
+
+const controls = new THREE.OrbitControls( camera, renderer.domElement )
 camera.position.z = -900
+controls.update()
 
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(ambientLight)
@@ -32,6 +35,30 @@ directionalLight.shadow.camera.right = 1000
 
 scene.add(directionalLight)
 
+const loadOneItem = (url) => {
+  return new Promise((resolve, reject) => {
+    if (url.indexOf('.mtl') !== -1) {
+      const mtlLoader = new THREE.MTLLoader()
+      mtlLoader.load(url, (obj) => {
+        resolve(obj)
+      })
+    } else if (url.indexOf('.obj') !== -1) {
+      const objLoader = new THREE.OBJLoader()
+      objLoader.load(url, (obj) => {
+        resolve(obj)
+      })
+    }
+  })
+}
+
+const loadAll = (urls) => {
+  return Promise.all(
+    urls.map(u => {
+      return loadOneItem(u)
+    })
+    )
+}
+
 const loadFiles = function (mtlUrl, objUrl) {
   return new Promise((resolve, reject) => {
     const objLoader = new THREE.OBJLoader()
@@ -46,66 +73,76 @@ const loadFiles = function (mtlUrl, objUrl) {
   })
 }
 
-let wave = null
-
 const uniforms = {
   time: { value: 1.0 },
   resolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
   solidColor: { value: new THREE.Color(0xffffff) }
 }
 
-loadFiles("wave.mtl", "wave.obj").then(function (obj) {
-  // obj.rotateX(Math.PI / 2)
-  // obj.rotateY(Math.PI)
-
-  const wave1 = new THREE.Mesh(obj)
-  wave1.position.set(0,0,0)
-  const wave2 = new THREE.Mesh(obj)
-  wave2.position.set(0,0,20)
-  const wave3 = new THREE.Mesh(obj)
-  wave3.position.set(0,0,40)
-
-  // wave = obj
-  const material = new THREE.ShaderMaterial({
-    uniforms: uniforms,
-    vertexShader: `
-    uniform float time;
-    void main () {
-      vec3 nPos = position;
-      nPos.y *= sin(nPos.x * 0.001 + time);
-      gl_Position = projectionMatrix * modelViewMatrix * vec4( nPos, 1.0 );
-    }
-    `,
-    fragmentShader: `
-    uniform vec3 solidColor;
-    void main (void) {
-      gl_FragColor = vec4(solidColor, 0.7);
-    }
-    `
-  })
-
-  material.uniforms.time.value = window.performance.now() * 0.03
-
-  obj.traverse(child => {
-    child.material = material
-    child.castShadow = true
-  })
-  scene.add(obj)
+const material = new THREE.ShaderMaterial({
+  uniforms: uniforms,
+  vertexShader: `
+  uniform float time;
+  void main () {
+    vec3 nPos = position;
+    nPos.y *= sin(nPos.x * 0.001 + time);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( nPos, 1.0 );
+  }
+  `,
+  fragmentShader: `
+  uniform vec3 solidColor;
+  void main (void) {
+    gl_FragColor = vec4(solidColor, 0.7);
+  }
+  `
 })
 
-const animate = function (timestamp) {
-  uniforms.time.value = timestamp / 1000;
-  camera.lookAt(scene.position)
-  renderer.render(scene, camera)
-  requestAnimationFrame(animate)
-}
+let delayItems = [
+]
 
-animate()
+loadAll([
+  './wave.obj'
+  ]).then((items) => {
+    let currentObject = items[0]
+    let totalObjects = 6
+    for (var i = 0; i < totalObjects; i++) {
+      let currentMatClone  = material.clone()
+      let currentClone = currentObject.clone()
+      currentClone.traverse((child) => {
+        if (child.type === 'Mesh') {
+          child.material = currentMatClone
+          child.castShadow = true
+        }
+      });
+      currentClone.position.set(0,-100 * i, 0)
+      let delay = 1000 * i
+      delayItems.push({
+        delay,
+        update: () => {
+          currentMatClone.uniforms.time.value = delay - window.performance.now() / 1000
+        }
+      })
+      setTimeout(function(){
+        scene.add(currentClone)
+       }, 1000 * i)
+    }
+  })
 
-window.addEventListener("resize", function () {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  uniforms.resolution.value.x = window.innerWidth;
-  uniforms.resolution.value.y = window.innerHeight;
-})
+  const animate = function (timestamp) {
+    uniforms.time.value = timestamp / 1000
+    delayItems.forEach(d => d.update())
+    camera.lookAt(scene.position)
+    controls.update()
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+  }
+
+  animate()
+
+  window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    uniforms.resolution.value.x = window.innerWidth
+    uniforms.resolution.value.y = window.innerHeight
+  })
